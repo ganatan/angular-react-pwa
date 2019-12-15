5b9e14513df8b2d78b4df1985d0d4ace91d0b33d
ganatan <dannyganatan@gmail.com>
2019-12-15 09:39:38 +0100

    update Angular CLI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,11 @@ import { HomeComponent } from './modules/general/home/home.component';
 import { NotFoundComponent } from './modules/general/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, },
   {
-    path: 'items',
+    path: 'httpclient',
     loadChildren: () => import('./modules/application/items/items.module').then(mod => mod.ItemsModule)
   },
-  {
+    {
     path: 'about',
     loadChildren: () => import('./modules/general/about/about.module').then(mod => mod.AboutModule)
   },
@@ -18,6 +17,19 @@ const routes: Routes = [
     path: 'contact',
     loadChildren: () => import('./modules/general/contact/contact.module').then(mod => mod.ContactModule)
   },
+  {
+    path: 'signin',
+    loadChildren: () => import('./modules/general/signin/signin.module').then(mod => mod.SigninModule)
+  },
+  {
+    path: 'components',
+    loadChildren: () => import('./modules/application/components/components.module').then(mod => mod.ComponentsModule)
+  },
+  {
+    path: 'services',
+    loadChildren: () => import('./modules/application/services/services.module').then(mod => mod.ServicesModule)
+  },
+  { path: '', component: HomeComponent, },
   { path: '**', component: NotFoundComponent }
 ];
 
