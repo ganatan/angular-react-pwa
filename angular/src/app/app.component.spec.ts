fac789a13c5894a996e45e1aa0aa0b152516e854
ganatan <dannyganatan@gmail.com>
2022-11-26 07:12:03 +0100

    Update Angular 15.0.1

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-pwa'`, () => {
+  it(`should have as title 'angular-bootstrap'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-pwa');
+    expect(app.title).toEqual('angular-bootstrap');
   });
 
 });
