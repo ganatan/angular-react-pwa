06e9fa55e9493baf6c53e3297ee979dec0f9b7ac
ganatan <dannyganatan@gmail.com>
2023-05-29 16:45:22 +0200

    Update Angular 16.0.3

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-pwa',
-    version: 'Angular 16.0.1',
+    version: 'Angular 16.0.3',
     bootstrap: 'Bootstrap 5.2.3',
     fontawesome: 'Font Awesome 6.4.0',
   }
