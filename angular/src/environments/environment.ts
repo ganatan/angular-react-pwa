60bd49db7065ba8bf97b5f1dc7530cf855d7228d
ganatan <dannyganatan@gmail.com>
2023-07-21 08:32:26 +0200

    Update Angular 16.1.6 & Angular CLI 16.1.5

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-pwa',
-    version: 'Angular 16.0.4',
+    version: 'Angular 16.1.6',
     bootstrap: 'Bootstrap 5.3.0',
     fontawesome: 'Font Awesome 6.4.0',
   }
