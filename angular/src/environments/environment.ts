3e6e4660fa3df4684f4c599a34aa6ddc34d54b27
ganatan <dannyganatan@gmail.com>
2024-07-15 10:40:30 +0200

    Update Angular 18.1.0

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     application:
     {
       name: 'angular-bootstrap',
-      version: 'Angular 18.0.6',
+      version: 'Angular 18.1.0',
       bootstrap: 'Bootstrap 5.3.3',
       fontawesome: 'Font Awesome 6.5.2',
     }
